e795ef3e1d72d8b80faefd6d0f6e07f7fa2d42df
Jose Iriarte <joseiriarte2102@gmail.com>
2023-01-08 23:04:58 -0400

    Scripts added

---
+++ R1/all_interfaces.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 GigabitEthernet2.py
+python3 GigabitEthernet4.py
---
+++ R2/all_configuration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 GigabitEthernet1.py
+python3 GigabitEthernet2.py
+python3 GigabitEthernet3.py 
+python3 create_static_routes.py
